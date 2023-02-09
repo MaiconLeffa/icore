612fc5459b5771d3fce8473be6e1990f627c3a31
MaiconLeffa <maicon_leffa@hotmail.com>
2023-02-09 14:37:06 -0300

    colocando link das redes sociais

--- components/footer/index.js
+++ components/footer/index.js
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { themes } from "styles/themes"
 import { Container, Rights, Wrapper } from "./styles"
 import { BsFacebook, BsInstagram, BsWhatsapp } from 'react-icons/bs'
+import Link from "next/link"
 
 const Footer = () =>
   <Container>
@@ -12,9 +13,17 @@ const Footer = () =>
         <Image alt='iCore' width={206} height={54} src='/logo.svg' />
 
         <div style={{ marginTop: 24, display: 'flex', justifyContent: 'center', gap: 24 }}>
-          <BsFacebook size={26} fill="#fff" />
-          <BsInstagram size={26} fill="#fff" />
-          <BsWhatsapp size={26} fill="#fff" />
+          <Link target='_blank' href='https://www.facebook.com/icore.solutions.torres'>
+            <BsFacebook size={26} fill="#fff" />
+          </Link>
+
+          <Link target='_blank' href="https://www.instagram.com/icore.solutions/">
+            <BsInstagram size={26} fill="#fff" />
+          </Link>
+
+          <Link target='_blank' passHref href="https://api.whatsapp.com/send?phone=5551936191086">
+            <BsWhatsapp size={26} fill="#fff" />
+          </Link>
         </div>
 
       </div>
