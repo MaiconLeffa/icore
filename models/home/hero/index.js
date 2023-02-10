507b0f550950ab7471e8c2a8358b38adfd369c22
MaiconLeffa <maicon_leffa@hotmail.com>
2023-02-10 20:05:44 -0300

    refatoração do carrosel de destaque

--- models/home/hero/index.js
+++ models/home/hero/index.js
@@ -1,34 +1,35 @@
-import Image from "next/image"
 import { useRef } from "react"
 import ReactElasticCarousel from "react-elastic-carousel"
-import { CarroselContainer, Container, Description, Emphasis, Title, TitleWrapper, Wrapper } from "./styles"
+import { CarroselContainer, Emphasis } from "./styles"
+import { BsArrowRightShort, BsArrowLeftShort } from 'react-icons/bs'
+import Slide from "./slide"
+
+const Slide3 = () =>
+  <Slide
+    image='/watch.png'
+    background='url(/slide3.jpg)'
+    title={<>Apple <Emphasis>Watch Ultra</Emphasis></>}
+    text={<>Conheça o Apple Watch mais versátil e duro na queda já criado. O Apple Watch Ultra é o relógio esportivo definitivo, com uma caixa de titânio robusta, GPS de precisão e dupla frequência, até 36 horas de bateria, conexão celular e três pulseiras especializadas feitas para atletas e para quem não vive sem adrenalina.</>}
+  />
 
 const Slide2 = () =>
-  <Container>
-    <TitleWrapper>
-      <Title>Apple <Emphasis>Watch Ultra</Emphasis></Title>
-      <Description>Conheça o Apple Watch mais versátil e duro na queda já criado. O Apple Watch Ultra é o relógio esportivo definitivo, com uma caixa de titânio robusta, GPS de precisão e dupla frequência, até 36 horas de bateria, conexão celular e três pulseiras especializadas feitas para atletas e para quem não vive sem adrenalina.</Description>
-    </TitleWrapper>
-    <Wrapper>
-      <Image draggable={false} alt="iCore" fill style={{ objectFit: 'contain' }} src='/watch.png' />
-    </Wrapper>
-  </Container>
+  <Slide
+    image='/iphone14.png'
+    background='url(/slide2.png)'
+    title={<> iPhone 14 < Emphasis > chegou!</Emphasis ></>}
+    text={<> Aqui na < Emphasis > iCore</Emphasis > o iPhone 14 já chegou! < br ></br > Com a nova câmera grande‑angular de nível profissional e melhorias no processamento de imagem, você faz fotos ainda mais espetaculares em qualquer cenário.Prepare‑se para ver a magia acontecer em pouca luz.</>}
+  />
 
 const Slide1 = () =>
-  <Container>
-    <TitleWrapper>
-      <Title>iPhone 14 <Emphasis>chegou!</Emphasis></Title>
-      <Description>Aqui na <Emphasis>iCore</Emphasis> o iPhone 14 já chegou!<br></br>Com a nova câmera grande‑angular de nível profissional e melhorias no processamento de imagem, você faz fotos ainda mais espetaculares em qualquer cenário. Prepare‑se para ver a magia acontecer em pouca luz.
-      </Description>
-    </TitleWrapper>
-    <Wrapper>
-      <Image draggable={false} alt="iCore" fill style={{ objectFit: 'contain' }} src='/iphone14.png' />
-    </Wrapper>
-  </Container>
+  <Slide
+    background='url(/exchange.jpg)'
+    title={<>Pegamos o <Emphasis>seu iPhone</Emphasis>  na compra de um <Emphasis>novo!</Emphasis></>}
+    text={<>Na <Emphasis>iCore</Emphasis> você deixa o seu iPhone antigo na troca!<br></br>e sai de iPhone novo.</>}
+  />
 
 const Hero = () => {
   const carouselRef = useRef(null);
-  const arr = [Slide1, Slide2]
+  const arr = [Slide1, Slide2, Slide3]
   const timer = 10000
   let resetTimeout;
 
@@ -48,7 +49,44 @@ const Hero = () => {
 
   return (
     <CarroselContainer>
-      <ReactElasticCarousel breakPoints={breakPoints} autoPlaySpeed={timer} ref={carouselRef} enableAutoPlay pagination={false} onNextEnd={onNextEnd}>
+      <ReactElasticCarousel
+        renderArrow={({ type, onClick, isEdge }) =>
+
+
+
+          <button
+
+            style={
+              {
+                width: 100,
+                zIndex: 1,
+                left: type === 'PREV' ? 0 : 'unset',
+                right: type === 'NEXT' ? 0 : 'unset',
+                position: 'absolute',
+                height: '100%',
+                background: 'transparent',
+                border: 0
+              }}
+            onClick={onClick} disabled={isEdge}>
+
+            {
+              type === 'PREV'
+                ? <BsArrowLeftShort fill="#fff" size={50} />
+                : <BsArrowRightShort fill="#fff" size={50} />
+            }
+
+
+          </button>
+
+        }
+        itemPadding={[0, 0]}
+        breakPoints={breakPoints}
+        //autoPlaySpeed={timer}
+        ref={carouselRef}
+        //enableAutoPlay
+        pagination={false}
+        onNextEnd={onNextEnd}
+      >
         {arr.map((Slide, index) => <Slide key={index} />)}
       </ReactElasticCarousel>
     </CarroselContainer>
--- models/home/hero/styles.js
+++ models/home/hero/styles.js
@@ -2,57 +2,11 @@ import styled from "styled-components";
 import { themes } from "styles/themes";
 
 export const CarroselContainer = styled.div`
-  padding: 24px; 
-  
-  @media(max-width: ${themes.breakpoints.mobile}){
-    padding: 24px 8px;
-  }
-`
-export const Container = styled.section`
-  display: flex;
-  height: 600px;
-  align-items: center;
-  gap: 24px;
-  width: 100%;
-
-  @media(max-width: ${themes.breakpoints.mobile}){
-    height: unset;
-    flex-direction: column-reverse;
-  }
-`
-export const Title = styled.h1`
-  color: #fff;
-  font-size: ${themes.sizes.lg};
-
-  @media(max-width: ${themes.breakpoints.mobile}){
-    font-size: 32px;
-    text-align: center;
-  }
-`
-export const Description = styled.p`
-  color: #fff;
-  font-size: 24px;
-  font-weight: normal;
-  
-  @media(max-width: ${themes.breakpoints.mobile}){
-    font-size: 18px;
-  }
-`
-export const TitleWrapper = styled.div`
-  width: 50%;
-
-  @media(max-width: ${themes.breakpoints.mobile}){
-    width: 100%;
-  }
- `
-export const Wrapper = styled.div`
+  padding: 0;
   position: relative;
-  width: 50%;
-  height: 100%;
 
-  @media(max-width: ${themes.breakpoints.mobile}){
-    width: 100%; 
-    height: 350px;
+  .rec-slider-container {
+    margin: 0 !important;
   }
 `
 export const Emphasis = styled.span`
